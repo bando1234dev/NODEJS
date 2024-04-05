a253eb1e49c0d1a05a808fb4ecf04a1bbd8e66a0
bando1234dev <cocthanhbeotft@gmail.com>
2024-04-05 13:57:19 +0700

    Section 2

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const { default: helmet } = require('helmet');
 const morgan = require('morgan');
 const app = express();
 
 app.use(morgan('dev'));
+app.use(helmet());
+app.use(compression());
 
 app.get('/', (req, res, next) => {
   return res.status(200).json({
     message: 'Welcome ban do1234!',
   });
 });
-
-console.log('123 : test git');
 
 module.exports = app;
