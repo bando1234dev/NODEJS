47c6bf8787446c6abb8e31c2d92e372f64b50e1b
bando1234dev <cocthanhbeotft@gmail.com>
2024-04-11 22:03:15 +0700

    Section 3

--- src/app.js
+++ src/app.js
@@ -2,10 +2,16 @@ const express = require('express');
 const { default: helmet } = require('helmet');
 const morgan = require('morgan');
 const app = express();
+const compression = require('compression');
 
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(compression());
+
+require('./dbs/init.mongodb');
+const { countConnect, checkOverload } = require('./helpers/check.connect');
+countConnect();
+// checkOverload();
 
 app.get('/', (req, res, next) => {
   return res.status(200).json({
